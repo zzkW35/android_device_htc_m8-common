2dd16a1f07f838251c0f4fec571ad47b424b03f0
Julian Veit <claymore1298@gmail.com>
2019-10-29 13:54:17 +0100

    Revert "m8-common: add ro.aicp.create.ors.file for OTA functionality"
    This reverts commit 232df35b285486ab3999365dcbb02e172ceec2e8.
    
    Change-Id: I1879ab8a8b0d2c1eef339449c3fce49b6ead4bb2

--- system_prop.mk
+++ system_prop.mk
@@ -10,10 +10,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
 
-# OTA - create openrecoveryscript manually
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aicp.create.ors.file=true
-
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.aicp.build.vendor_security_patch=2016-07-01
